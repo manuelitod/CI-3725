57d8ad1797b79e971567e0da04fc06905caa2f62
Manuel Rodriguez <mr.1311223@gmail.com>
2018-06-02 21:11:20 -0400

    Agregados '''

--- parser.py
+++ parser.py
@@ -6,61 +6,60 @@ from lexNEO import *
 # Símbolo inicial
 
 def p_inicio(p):
-	INICIO :  TkWith DEC TkBegin INSTR TkEnd
-			| TkBegin INSTR TkEnd
+	'''INICIO :  TkWith DEC TkBegin INSTR TkEnd
+			| TkBegin INSTR TkEnd'''
 
 # Lista de Declaraciones
 
 def p_dec(p):
-	DEC : DEC TkVar IDENT TkDosPuntos TIPO
-		| TkVar IDENT TkDosPuntos TIPO
+	'''DEC : DEC TkVar IDENT TkDosPuntos TIPO
+		| TkVar IDENT TkDosPuntos TIPO'''
 
 # Identificadores
 
 def p_ident(p):
-	IDENT :   IDENT TkComa TkId
+	'''IDENT :   IDENT TkComa TkId
 			| TkId
-			| ASIG
+			| ASIG'''
 
 # Tipos de variables
 
 def p_tipo(p):
-	TIPO :    TkInt
+	'''TIPO :    TkInt
 			| TkChar
 			| TkBool
-			| TkArray
+			| TkArray'''
 
 # Instrucciones generales
 
 def p_instr(p):
-	INSTR :   SEC
-			| INSTR1
-
+	'''INSTR :   SEC
+			| INSTR1'''
 # Secuenciacion
 
 def p_sec(p):
-	SEC : INSTR INSTR1
+	'''SEC : INSTR INSTR1'''
 
 # Instrucciones
 
 def p_instr1(p):
-	INSTR1 :  ASIG
+	'''INSTR1 :  ASIG
 			| I_O
 			| COND
 			| DETER
 			| INDETER
-			| INICIO
+			| INICIO'''
 
 # Asignacion 
 
 def p_asig(p):
-	ASIG :    TkId TkAsignacion ARIT TkPuntoYComa
-			| TkId TkAsignacion BOOL TkPuntoYComa
+	'''ASIG :    TkId TkAsignacion ARIT TkPuntoYComa
+			| TkId TkAsignacion BOOL TkPuntoYComa'''
 
 # Expresiones Aritmeticas
 
 def p_arit(p):
-	ARIT :    TkNum
+	'''ARIT :    TkNum
 			| TkId
 			| TkParAbre ARIT TkParCierra
 			| ARIT TkSuma ARIT
@@ -70,15 +69,15 @@ def p_arit(p):
 			| ARIT TkMod ARIT
 			| TkValorAscii TkCaracter
 			| TkNum TkSiguienteCar
-			| TkNum TkAnteriorCar
+			| TkNum TkAnteriorCar'''
 
 def p_nega(p):
-	ARIT :	  TkResta ARIT %prec NEGA 
+	'''ARIT :	  TkResta ARIT %prec NEGA'''
 
 # Expresiones Booleanas
 
 def p_bool(p):
-	BOOL : 	  BOOL TkConjuncion BOOL
+	'''BOOL : 	  BOOL TkConjuncion BOOL
 			| BOOL TkDisyuncion BOOL
 			| BOOL TkMenor BOOL
 			| BOOL TkMenorIgual BOOL
@@ -88,56 +87,56 @@ def p_bool(p):
 			| TkParAbre BOOL TkParCierra
 			| TkNegacion BOOL
 			| TkFalse
-			| TkTrue
+			| TkTrue'''
 
 # Expresiones con caracteres
 
 def p_char(p):
-	CHAR : 	  TkId
+	'''CHAR : 	  TkId
 			| TkCaracter
 			| CHAR TkConcatenacion CHAR
 			| TkCaracter TkSiguienteCar
-			| TkCaracter TkAnteriorCar
+			| TkCaracter TkAnteriorCar'''
 
 # Condicionales general
 
 def p_cond(p):
-	COND : 	  TkIf TkHacer INSTR COND0
+	'''COND : 	  TkIf TkHacer INSTR COND0'''
 
 # Condicionales 
 
 def p_cond0(p):
-	COND0 :   TkOtherwise TkHacer INSTR
-			| TkEnd
+	'''COND0 :   TkOtherwise TkHacer INSTR
+			| TkEnd'''
 
 # Iteracion indeterminada
 
 def indeter(p):
-	INDETER : TkWhile BOOL TkHacer INSTR TkEnd
+	'''INDETER : TkWhile BOOL TkHacer INSTR TkEnd'''
 
 # Iteracion determinada general
 
 def deter(p):
-	DETER :   TkFor TkId TkFrom ARIT TkTo ARIT DETER0
+	'''DETER :   TkFor TkId TkFrom ARIT TkTo ARIT DETER0'''
 
 # Iteracion determinada
 
 def deter0(p):
-	DETER0 :  TkStep ARIT TkHacer INSTR TkEnd
-			| TkHacer INSTR TkEnd
+	'''DETER0 :  TkStep ARIT TkHacer INSTR TkEnd
+			| TkHacer INSTR TkEnd'''
 
 # Entrada y salida
 
 def i_o(p):
-	I_O : 	  TkRead TkId
-			| TkBegin EXPR
+	'''I_O : 	  TkRead TkId
+			| TkBegin EXPR'''
 
 # Expresiones en general
 
 def expr(p):
-	EXPR : 	  CHAR
+	'''EXPR : 	  CHAR
 			| BOOL
-			| ARIT
+			| ARIT'''
 
 precedence = (
 	('left', 'TkConjuncion', 'TkDisyuncion', 'TkSiguienteCar', 'TkAnteriorCar', 'TkConcatenacion'),
